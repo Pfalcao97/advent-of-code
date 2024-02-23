93ea6d01d92903600473782577c010e7e2ecd872
Pedro Falcao <pedrofalcao97@gmail.com>
2024-02-22 23:59:13 -0300

    problem with regex

--- 2023/day-1/src/main.rs
+++ 2023/day-1/src/main.rs
@@ -1,10 +1,9 @@
 use std::fs;
 use regex::Regex;
-use std::collections::HashMap;
 
 fn main() {
     
-    let  contents = fs::read_to_string("fake-input.txt")
+    let  contents = fs::read_to_string("input.txt")
                                 .expect("input.txt not found."); 
 
     let numbers:Vec<u16> = contents
@@ -20,26 +19,32 @@ fn extractor(phrase: &str) -> u16 {
 
     let mut s = String::from("");
 
-    for n in Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine")
+    // I'm having a hard time capturing cases where the end of a match
+    // coincides with the begining of another, like in:
+    // twoeightnfhdbndrtzltninehdtkheight4eightwolpr 
+    // which ignored the last number (two), in favor of the "eight".
+
+    let numbers:Vec<String> = Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine")
                             .unwrap()
                             .find_iter(phrase)
                             .map(|m| m.as_str().to_lowercase())
-                            .map(|num_str| num_str) {
-     println!("{}",n);
-                            }
-
-    let numbers: Vec<&str> = Vec::new();
-    //let numbers:Vec<&str> = Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine")
-    //                        .unwrap()
-    //                        .find_iter(phrase)
-    //                        .map(|m| m.as_str().to_lowercase())
-    //                        .map(|num_str| text_2_number.get(&num_str).expect("Not a number.").as_str())
-    //                        .collect();
+                            .map(|num_str| match num_str.as_str() {
+                                "one" => String::from("1"),
+                                "two" => String::from("2"),
+                                "three" => String::from("3"),
+                                "four" => String::from("4"),
+                                "five" => String::from("5"),
+                                "six" => String::from("6"),
+                                "seven" => String::from("7"),
+                                "eight" => String::from("8"),
+                                "nine" => String::from("9"),
+                                _ => String::from(num_str)
+                            })
+                            .collect();
                         
     let first = numbers[0].chars().next()
                                 .expect("Empty string.");
 
-    println!("{}", first);
     let last = numbers.last().expect("No numbers found.")
                                     .chars().next().expect("Empty string.");
 

3d56f94812983096765c5d8702140cf4f392cd89
Pedro Falcao <pedrofalcao97@gmail.com>
2024-04-11 23:21:58 -0300

    finished second part

--- 2022/day-4/src/main.rs
+++ 2022/day-4/src/main.rs
@@ -5,39 +5,34 @@ fn main() {
     let  contents = fs::read_to_string("input.txt")
                                 .expect("input.txt not found."); 
 
+    let number_of_lines = contents.lines().count();
+    println!("Number of lines: {}", number_of_lines);
 
-    let mut counter:i16 = 0;
+    let mut counter:u8 = 0;
     for line in contents.lines() {
 
         let groups:Vec<&str> = line.split(',').collect();
 
-        let first:Vec<i16> = groups[0]
+        let first:Vec<u8> = groups[0]
                                 .split('-')
                                 .collect::<Vec<&str>>()
                                 .iter()
                                 .filter_map(|s| s.parse().ok())
                                 .collect();
 
-        let second:Vec<i16> = groups[1]
+        let second:Vec<u8> = groups[1]
                                 .split('-')
                                 .collect::<Vec<&str>>()
                                 .iter()
                                 .filter_map(|s| s.parse().ok())
                                 .collect();
 
-
-
-        if first[0] >= second[0] && first[1] <= second[1] {     
-            counter += 1;              
-        };
-
-
-        if first[0] < second[0] && first[1] >= second[1] {
+        if first[1] < second[0] || first[0] > second[1] || second[1] < first[0] || second[0] > first[1] {
+            // I've found it easier to subtract those that don't overlap than to think on edge cases that did.
             counter += 1;
-        } else if first[0] == second[0]  && first[1] > second[1] {
-            counter += 1;
-        };
+        }
     };
 
     println!("Counter: {}", counter);
+    println!("Groups that don't overlap: {}", number_of_lines - counter as usize);
 }

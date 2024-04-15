fe609b09d3baef73e52d515f2c4e8ac67a711a49
Pedro Falcao <pedrofalcao97@gmail.com>
2024-04-14 22:01:09 -0300

    finished first part

---
+++ 2022/day-5/src/main.rs
@@ -0,0 +1,73 @@
+use std::fs;
+use std::collections::HashMap;
+
+fn create_stacks(stack_vector:&mut Vec<(usize, char)>) -> HashMap<usize, Vec<char>> {
+
+    let qty_columns = stack_vector.last().unwrap().0;
+    let mut stacks: HashMap<usize, Vec<char>> = HashMap::new();
+
+    for _i in 0..qty_columns {
+        stack_vector.pop();
+    };
+
+    let clean_stacks:Vec<&(usize, char)> = stack_vector.iter().filter(|x| x.1 != ' ').collect();
+
+    for letter_box in clean_stacks.iter().rev() {
+
+        if !stacks.contains_key(&letter_box.0) {
+            stacks.insert(letter_box.0, vec![letter_box.1]);
+        } else {
+            stacks.get_mut(&letter_box.0).unwrap().append(&mut vec![letter_box.1]);
+        };
+    };
+
+    stacks
+}
+
+fn main() {
+    
+    let  contents = fs::read_to_string("input.txt")
+                                .expect("input.txt not found."); 
+
+    let parts:Vec<&str> = contents.split("\n\n").collect();
+
+    let mut stacks_aux = Vec::new();
+    for line in parts[0].lines() {
+        for c in line.chars().enumerate() {
+            if (c.0 + 3) % 4 == 0 {
+                stacks_aux.push(((c.0 + 3)/4, c.1));
+            };
+        };
+    };
+
+    let mut stacks = create_stacks(&mut stacks_aux);
+    //println!("Starting as: {:?}", stacks);
+
+    for line in parts[1].lines() {
+
+        let parsed_moves:Vec<&str> = line.split(" from ").collect();
+
+        let qty:usize = parsed_moves[0].split(" ").collect::<Vec<&str>>()[1].parse().unwrap();
+        let from:usize = parsed_moves[1].split(" to ").collect::<Vec<&str>>()[0].parse().unwrap();
+        let to:usize = parsed_moves[1].split(" to ").collect::<Vec<&str>>()[1].parse().unwrap();
+
+        for _i in 0..qty {
+
+            let mut start = stacks.get(&from).unwrap().clone();
+            let aux = start.pop().unwrap();
+
+            stacks.insert(from, start.to_vec());
+
+            let mut finish = stacks.get(&to).unwrap().clone();
+            finish.push(aux);
+
+            stacks.insert(to, finish.to_vec());
+        }
+         
+        //println!("After moving {}, from {} to {}: {:?}", qty,from,to, stacks);
+    }
+
+    for k in stacks.keys() {
+        println!("Last item from {}: {}", k, stacks.get(k).unwrap().last().unwrap());
+    }
+}
